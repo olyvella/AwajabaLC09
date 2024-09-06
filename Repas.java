18ed04f57f17d0243a06970678076f9b2d2ba068
olyvella <144382947+olyvella@users.noreply.github.com>
2024-09-06 16:20:04 +0200

    Update Repas.java

--- Repas.java
+++ Repas.java
@@ -1,7 +1,11 @@
 public class Repas.java {
+
+import java.time.LocalDate;
+
   public static void main( String [] args){
 
-    int numero , LocalDate date ; 
+    int numero , 
+    LocalDate date ; 
 
   }
 }
